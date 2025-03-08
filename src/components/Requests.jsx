80f40aa50208fb97c6c18bd079668edce83c54b7
Mounika-Gummidi <mounika.gummidi68@gmail.com>
2025-03-08 10:16:26 +0530

    accepting and rejecting the requests

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -2,11 +2,25 @@ import axios from "axios";
 import {BASE_URL} from "../utils/constants";
 import { useEffect } from "react";
 import { useDispatch,useSelector } from "react-redux";
-import { addRequests } from "../utils/requestSlice";
+import { addRequests, removeRequest } from "../utils/requestSlice";
 
 const Requests = () =>{
   const requests = useSelector((store)=>store.requests);
   const dispatch = useDispatch();
+
+  const reviewRequests = async (status , _id) =>{
+    try {
+      const res= await axios.post(BASE_URL+"/request/review/"+status+"/"+_id,
+        {},{withCredentials:true},
+      );
+      console.log(res);
+      dispatch(removeRequest(_id));
+    } catch (err) {
+      console.log(err);
+    }
+    
+  }
+
   const fetchRequests = async () => {
     try{
       const res = await axios.get(BASE_URL+"/user/requests/received",{withCredentials: true});
@@ -21,15 +35,18 @@ const Requests = () =>{
     fetchRequests();
   },[]);
 
+  if(!requests) return;
+  if(requests.length===0) return <h1 className="flex justify-center text-2xl">No Requests Found!!</h1>
+
   return requests && (
-    <div className="">
+    <div>
       <h1 className="text-center text-bold text-2xl my-5 text-white">Requests</h1>
       {requests.map((request) => 
       {
         const {_id,firstName,lastName,photoUrl,age,gender,about} = request.fromUserId;
         return (
           <div key={_id} className="my-3">
-            <ul className="list bg-base-200 mx-auto max-w-3xl rounded-box shadow-md">
+            <ul className="list bg-base-300 mx-auto max-w-2xl rounded-box shadow-md">
             <li className="list-row">
               <div><img className="size-10 rounded-box" src={photoUrl}/></div>
               <div>
@@ -39,10 +56,10 @@ const Requests = () =>{
               <p className="list-col-wrap text-xs">
                 {about}
               </p>
+
+                  <button className="btn btn-active btn-primary " onClick={()=>reviewRequests("accepted",request._id)}>Accept</button>
+                  <button className="btn btn-active btn-secondary"onClick={()=>reviewRequests("rejected",request._id)}>Reject</button>
               
-              <button className="btn btn-square btn-ghost">
-                <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
-              </button>
             </li>
           </ul>
           </div>
--- src/utils/requestSlice.js
+++ src/utils/requestSlice.js
@@ -5,8 +5,10 @@ const requestSlice = createSlice({
   initialState:null,
   reducers:{
     addRequests:(state,action)=>{return action.payload;},
-    removeRequests:()=>{return null;}
+    removeRequest:(state, action)=>{const newArray=state.filter(r=>r._id !== action.payload);
+      return newArray;
+    }
   }
 })
-export const {addRequests,removeRequests} = requestSlice.actions;
+export const {addRequests,removeRequest} = requestSlice.actions;
 export default requestSlice.reducer;
